e09ad1017f1e2d6d008cf0ab3f7fce1436e75cc9
BerkM125 <49427416+BerkM125@users.noreply.github.com>
2020-03-28 21:22:26 -0700

    Update JBStyling.js

--- JBStyling.js
+++ JBStyling.js
@@ -53,6 +53,10 @@ function startRuler() {
             if (mapClickState === 0) {
                 mymap.on('mousemove', function(e) {
                     points = [circle.getLatLng(), circle2.getLatLng()];
+                    points[0].lat-=5;
+                    points[0].lon-=5;
+                    points[1].lat+=5;
+                    points[1].lat+=5;
                     if (linestate === 0) {
                         mymap.removeLayer(line2);
                         line = L.polyline(points, {
@@ -92,6 +96,10 @@ function startRuler() {
             if (mapClickState === 0) {
                 mymap.on('mousemove', function(e) {
                     points = [circle.getLatLng(), circle2.getLatLng()];
+                    points[0].lat-=5;
+                    points[0].lon-=5;
+                    points[1].lat+=5;
+                    points[1].lat+=5;
                     if (linestate === 0) {
                         mymap.removeLayer(line2);
                         line = L.polyline(points, {
