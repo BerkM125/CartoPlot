c543c1fae39e9825224185c409f1371442aecf1c
Diamond <DiamondMiner88@users.noreply.github.com>
2020-03-29 09:51:05 -0700

    beautify + remove set coords specifically if plot coords are 0

--- JBStyling.js
+++ JBStyling.js
@@ -5,126 +5,27 @@ var linestate, linestate2;
 var osm = L.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png').addTo(mymap);
 
 function plotdraggablepoint() {
-    var locationfield = document.getElementById("locationfield");
-    var latitude, longitude;
-    if (document.getElementById("locationfield2").value != 0 && document.getElementById("locationfield3").value != 0) {
-        latitude = document.getElementById("locationfield2").value;
-        longitude = document.getElementById("locationfield3").value;
-    } else {
-        latitude = 47.5301;
-        longitude = -122.0326;
-    }
-    mymap.setView([latitude, longitude], 13);
-    bindstring = locationfield.value;
-    var marker = L.marker([latitude, longitude], {
-        riseOnHover: true,
-        draggable: true
-    }).addTo(mymap);
-    marker.bindTooltip(bindstring, {
-        className: 'tooltipclass'
-    }).openTooltip();
+  var locationfield = document.getElementById("locationfield");
+  var latitude, longitude;
+  if (document.getElementById("locationfield2").value == "" || document.getElementById("locationfield3").value == "") {
+    alert("You need to put both latitude and longitude in order to plot");
+    return;
+  }
+  latitude = document.getElementById("locationfield2").value;
+  longitude = document.getElementById("locationfield3").value;
+
+  mymap.setView([latitude, longitude], 13);
+  bindstring = locationfield.value;
+  var marker = L.marker([latitude, longitude], {
+    riseOnHover: true,
+    draggable: true
+  }).addTo(mymap);
+  marker.bindTooltip(bindstring, {
+    className: 'tooltipclass'
+  }).openTooltip();
 }
 
 function startRuler() {
-    //Circle pick-and-drop ruler
-    /*document.getElementById("onRulerEnabled").style.color = "rgba(255,255,255,1)";
-
-    var circle = L.circle([47.6163, -122.0356], 600, {
-        riseOnHover: true
-    }).addTo(mymap);
-    var circle2 = L.circle([47.5301, -122.0326], 600, {
-        riseOnHover: true
-    }).addTo(mymap);
-    var line, line2;
-    var points = [circle.getLatLng(), circle2.getLatLng()];
-    mymap.setView(circle.getLatLng(), 12);
-    line = L.polyline(points, {
-        color: 'red',
-        weight: 3,
-        opacity: 0,
-        smoothFactor: 1
-    }).addTo(mymap);
-    line2 = L.polyline(points, {
-        color: 'red',
-        weight: 3,
-        opacity: 0,
-        smoothFactor: 1
-    }).addTo(mymap);
-    circle.on({
-        mousedown: function() {
-            if (mapClickState === 0) {
-                mymap.on('mousemove', function(e) {
-                    points = [circle.getLatLng(), circle2.getLatLng()];
-                    if (linestate2 === 0) {
-                        mymap.removeLayer(line);
-                        mymap.removeLayer(line2);
-                        line = L.polyline(points, {
-                            color: 'red',
-                            weight: 3,
-                            opacity: 0.5,
-                            smoothFactor: 1
-                        }).addTo(mymap);
-                        linestate2 += 1;
-                    } else {
-                        mymap.removeLayer(line);
-                        line2 = L.polyline(points, {
-                            color: 'red',
-                            weight: 3,
-                            opacity: 0.5,
-                            smoothFactor: 1
-                        }).addTo(mymap);
-                        linestate2 = 0;
-                    }
-                    circle.setLatLng(e.latlng);
-                });
-                mapClickState = 1;
-            } else if (mapClickState === 1) {
-                mapClickState = 0;
-                mymap.removeEventListener('mousemove');
-                circle.bindTooltip(L.GeometryUtil.length([circle.getLatLng(), circle2.getLatLng()]).toString() + " Meters", {
-                    className: 'tooltipclass'
-                }).openTooltip()
-            }
-        }
-    });
-    circle2.on({
-        mousedown: function() {
-            if (mapClickState === 0) {
-                mymap.on('mousemove', function(e) {
-                    points = [circle.getLatLng(), circle2.getLatLng()];
-                    if (linestate === 0) {
-                        mymap.removeLayer(line);
-                        mymap.removeLayer(line2);
-                        line = L.polyline(points, {
-                            color: 'red',
-                            weight: 3,
-                            opacity: 0.5,
-                            smoothFactor: 1
-                        }).addTo(mymap);
-                        linestate += 1;
-                    } else {
-                        mymap.removeLayer(line);
-                        line2 = L.polyline(points, {
-                            color: 'red',
-                            weight: 3,
-                            opacity: 0.5,
-                            smoothFactor: 1
-                        }).addTo(mymap);
-                        linestate = 0;
-                    }
-                    circle2.setLatLng(e.latlng);
-                });
-                mapClickState = 1;
-            } else if (mapClickState === 1) {
-                mapClickState = 0;
-                mymap.removeEventListener('mousemove');
-                circle2.bindTooltip(L.GeometryUtil.length([circle.getLatLng(), circle2.getLatLng()]).toString() + " Meters", {
-                    className: 'tooltipclass'
-                }).openTooltip()
-            }
-        }
-    }); */
-
   document.getElementById("onRulerEnabled").style.color = "rgba(255,255,255,1)";
 
   let point1;
@@ -152,7 +53,7 @@ function startRuler() {
         smoothFactor: 1
       }).addTo(mymap).bindPopup((Math.round(L.GeometryUtil.length([point1.getLatLng(), point2.getLatLng()]) * 100) / 100).toString() + " Meters", {
         className: 'popUp',
-          closeOnClick: false,
+        closeOnClick: false,
         autoClose: false
       }).openPopup();
 
@@ -188,17 +89,17 @@ function removeAllLayers() {
 
 // Close sidebar when ESC pressed
 document.onkeydown = function(event) {
-    event = event || window.event;
-    if (event.keyCode == 27) {
-        document.getElementById("slider").checked = false;
-    }
+  event = event || window.event;
+  if (event.keyCode == 27) {
+    document.getElementById("slider").checked = false;
+  }
 };
 
 function sidebarUpdate() {
-    slider = document.getElementById("slider").checked;
-    if (slider) {
-        document.getElementById("togglerLabel").innerHTML = "✖ Tools";
-    } else {
-        document.getElementById("togglerLabel").innerHTML = "☰ Tools";
-    }
+  slider = document.getElementById("slider").checked;
+  if (slider) {
+    document.getElementById("togglerLabel").innerHTML = "✖ Tools";
+  } else {
+    document.getElementById("togglerLabel").innerHTML = "☰ Tools";
+  }
 }
