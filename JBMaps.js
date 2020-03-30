12ca2e585cea99ccc9b55b5cbc38e8a643afe5c1
Diamond <DiamondMiner88@users.noreply.github.com>
2020-03-30 09:34:56 -0700

    comment out saving + loading maps

--- JBMaps.js
+++ JBMaps.js
@@ -204,39 +204,39 @@ function sidebarUpdate() {
     }
 }
 
-var geojson = [];
-
-function saveMap() {
-  // getCircularReplacer is taken from https://docs.w3cub.com/javascript/errors/cyclic_object_value/
-  const getCircularReplacer = () => {
-    const seen = new WeakSet();
-    return (key, value) => {
-      if (typeof value === "object" && value !== null) {
-        if (seen.has(value)) {
-          return;
-        }
-        seen.add(value);
-      }
-      return value;
-    };
-  };
-
-  mymap.eachLayer(function(layer) {
-    geojson.push(layer);
-  });
-  geojson = JSON.stringify(geojson, getCircularReplacer());
-}
-
-function loadMap() {
-  mymap.eachLayer(function(layer) {
-    mymap.removeLayer(layer);
-  });
-
-  let jsondata = JSON.parse(geojson);
-  jsondata.forEach((element) => {
-    console.log(element)
-    layer = L.GeoJSON.geometryToLayer(element);
-    console.log(layer);
-    layer.addTo(mymap);
-  });
-}
+// var geojson = [];
+//
+// function saveMap() {
+//   // getCircularReplacer is taken from https://docs.w3cub.com/javascript/errors/cyclic_object_value/
+//   const getCircularReplacer = () => {
+//     const seen = new WeakSet();
+//     return (key, value) => {
+//       if (typeof value === "object" && value !== null) {
+//         if (seen.has(value)) {
+//           return;
+//         }
+//         seen.add(value);
+//       }
+//       return value;
+//     };
+//   };
+//
+//   mymap.eachLayer(function(layer) {
+//     geojson.push(layer);
+//   });
+//   geojson = JSON.stringify(geojson, getCircularReplacer());
+// }
+//
+// function loadMap() {
+//   mymap.eachLayer(function(layer) {
+//     mymap.removeLayer(layer);
+//   });
+//
+//   let jsondata = JSON.parse(geojson);
+//   jsondata.forEach((element) => {
+//     console.log(element)
+//     layer = L.GeoJSON.geometryToLayer(element);
+//     console.log(layer);
+//     layer.addTo(mymap);
+//   });
+// }
